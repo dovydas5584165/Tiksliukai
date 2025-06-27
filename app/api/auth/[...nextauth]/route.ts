a6dfcd022d80744e242a0204521ed92dd7094d11
Dovydas Žilinskas <dovydaszilinskas@Dovydass-MacBook-Air.local>
2025-06-27 15:47:27 +0300

    Fix database connection config

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
-import { authOptions } from "@/lib/authOptions";
+// app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
 const handler = NextAuth(authOptions);
 
